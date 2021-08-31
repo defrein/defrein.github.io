04c68a187bf5b6c74eaf842debdd269e8049c71f
Wahni Adnani <75670541+defrein@users.noreply.github.com>
2021-08-31 15:47:31 +0800

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+const getCodeInput = document.querySelector(".input-code input");
+const getCodeBtn = document.querySelector(".input-code button");
+const secretBtn = document.querySelector(".secret-btn a");
+const secretP = document.querySelector(".secret-btn p");
+let width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+console.log(width);
+if (width < 800) {
+    alert("This site doesn't support mobile devices!");
+    window.close();
+}
+
+getCodeBtn.onclick = () => {
+    let theCode = getCodeInput.value;
+    if (theCode === "petrichor") {
+        secretBtn.classList.add("appear");
+        secretBtn.setAttribute('href', "https:defrein.github.io/assets/mx27082020/ily.html");
+        secretP.classList.remove("appear");
+    } else {
+        secretP.classList.add("appear");
+        secretBtn.classList.remove("appear");
+    }
+    getCodeInput.value = '';
+}
