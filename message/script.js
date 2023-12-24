e087f00f9b17d8a16b4617d56d76b17ef0b08aed
Wahni Adnani (DEF) <75670541+defrein@users.noreply.github.com>
2023-12-24 19:53:46 +0800

    Update script.js

--- message/script.js
+++ message/script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-    question.innerHTML = "yay, i know you like me!";
+    question.innerHTML = "yay, i know you like me >//<";
     gif.src =
         "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
